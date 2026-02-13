adefcbc313c03ecba1fdb7fd0a3e74843d55b66d
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2026-02-13 12:31:01 +0100

    Update Exercise1.java
    exercise completed

--- src/arrays/exercises/Exercise1.java
+++ src/arrays/exercises/Exercise1.java
@@ -2,7 +2,7 @@ package arrays.exercises;
 
 /**
  * Exercise 1: Single-Dimensional Array
- * TODO:
+
  * 1. Create an array of 5 integers.
  * 2. Traverse and print elements.
  * 3. Find sum of all elements.
@@ -11,6 +11,18 @@ public class Exercise1
 {
     public static void main(String[] args)
     {
-        // TODO: Implement exercise
+        int[] numbers = { 10, 20, 30, 40, 50 }; // an array of 5 integers.
+        
+       // Traverse and print elements.
+        System.out.println("Array elements:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+      // Find sum of all elements
+        int sum = 0;
+        for (int num : numbers) {
+            sum = sum + num;
+        }
+        System.out.println("Sum of all elements: " + sum);
     }
 }
