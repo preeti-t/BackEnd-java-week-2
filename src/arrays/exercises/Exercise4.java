6f4f107eaf0deb8b0d9cc55ce29ac97d669aea23
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2026-02-13 12:34:30 +0100

    Update Exercise4.java
    Exercise Completed

--- src/arrays/exercises/Exercise4.java
+++ src/arrays/exercises/Exercise4.java
@@ -15,7 +15,20 @@ public class Exercise4
 {
     public static void main(String[] args)
     {
-        // TODO: Implement exercise
+        int[] arr = { 100, 20, 13, 40, 35, 29 }; // Created  an array of 6 integers.
+
+        Arrays.sort(arr);                                         // array sorted
+        System.out.println("Sorted: " + Arrays.toString(arr));
+        
+        int[] copy = Arrays.copyOf(arr, arr.length);             //Copied into another array
+        System.out.println("Copy: " + Arrays.toString(copy));
+
+        int index = Arrays.binarySearch(arr, 40);     //searched for value 40
+        System.out.println("Index of 40: " + index);
+
+         int[] arr2 = {70, 20, 30, 35, 60, 80};                // Created a new array arr2 and compared with first
+        System.out.println("Arrays equal? " + Arrays.equals(arr, arr2));
+
     }
 }
 
