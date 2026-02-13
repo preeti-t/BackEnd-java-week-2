3d6487cd88a19683cbf4b59f0b92822892f301bb
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2026-02-13 12:36:20 +0100

    Update Exercise6.java
    Exercise Completed

--- src/arrays/exercises/Exercise6.java
+++ src/arrays/exercises/Exercise6.java
@@ -2,7 +2,7 @@ package arrays.exercises;
 
 /**
  * Exercise 6: Binary Search
- * TODO:
+ * TO DO:
  * 1. Create and sort an array.
  * 2. Ask user for a number.
  * 3. Implement binary search.
@@ -10,10 +10,27 @@ package arrays.exercises;
  */
 
 import java.util.Arrays;
+import java.util.Scanner;
 public class Exercise6
 {
     public static void main(String[] args)
     {
-        // TODO: Implement exercise
+        int[] arr = { 110, 210, 13, 40, 29 }; // Created  an array of integers.
+
+        Arrays.sort(arr);                                         // Array sorted
+        System.out.println("Sorted: " + Arrays.toString(arr));
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int key = scanner.nextInt();
+
+        scanner.close();
+        int index = Arrays.binarySearch(arr, key);      // Implemented binary search.
+
+        if (index > -1)                                            //Result
+            System.out.println("Element found at index " + index);
+        else
+            System.out.println("Element not found");
     }
 }
