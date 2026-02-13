38c76255ab0dab279895820791c2763efe4a7941
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2026-02-13 12:44:21 +0100

    Update Exercise1.java
    Exercise Completed

--- src/methods/exercises/Exercise1.java
+++ src/methods/exercises/Exercise1.java
@@ -1,11 +1,8 @@
 package methods.exercises;
 
-import methods.examples.Example1;
-
 /**
  * Exercise 1: Basic Methods
  * -------------------------
- * TODO:
  * 1. Create a method `cube(int num)` that returns the cube of a number.
  * 2. Create a method `sayHello(String name)` that prints "Hello, <name>".
  * 3. Call both methods from main().
@@ -13,28 +10,32 @@ import methods.examples.Example1;
 
 public class Exercise1
 {
-    public static void main(String[] args)
+  public static void main(String[] args)
     {
+    Exercise1 obj = new Exercise1();
 
-        Example1.staticGreeting();  //call a public method from a different class
+    // Example usage
+    System.out.println("Square of 4 = " + obj.square(4));
+    System.out.println("Cube of 4 = " + obj.cube(4));
 
-        Exercise1 obj = new Exercise1();
-
-        // Example usage
-        System.out.println("Square of 4 = " + obj.square(4));
-
-        // TODO: Call cube() and sayHello() here
+    obj.sayHello("Preeti");
     }
 
     // Example method: square
-    public int square(int num)
-    {
-        return num * num;
+    public int square(int num){
+    return num * num;
     }
 
     // TODO: Write cube(int num)
+    public int cube(int num){
+    return num* num* num;
+    }
 
     // TODO: Write sayHello(String name)
 
+    public void sayHello(String name){
+    System.out.println("Hello, " + name);
+    }
+    
 }
 
