fc0a73e94286d31ad6e9adf73d6b674ab7146e72
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2026-02-13 12:33:09 +0100

    Update Exercise3.java
    Exercise Completed

--- src/arrays/exercises/Exercise3.java
+++ src/arrays/exercises/Exercise3.java
@@ -2,7 +2,6 @@ package arrays.exercises;
 
 /**
  * Exercise 3: Jagged Array
- * TODO:
  * 1. Create a jagged array with 3 rows.
  * 2. Assign values manually.
  * 3. Traverse and print array.
@@ -12,6 +11,20 @@ public class Exercise3
 {
     public static void main(String[] args)
     {
-        // TODO: Implement exercise
+         int[][] jagged = new int[3][];
+        jagged[0] = new int[]{1, 2, 3};
+        jagged[1] = new int[]{4, 5};
+        jagged[2] = new int[] { 6 };
+        int sum = 0;
+
+        for (int i = 0; i < jagged.length; i++)
+        {
+            for (int j = 0; j < jagged[i].length; j++) {
+                System.out.print(jagged[i][j] + " ");
+                sum = sum + jagged[i][j];
+            }
+            System.out.println();
+        }
+        System.out.println("Sum of all elements: " + sum);
     }
 }
