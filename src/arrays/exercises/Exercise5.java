02881514cd73aa54a3981c787323c93dbf6822ca
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2026-02-13 12:35:20 +0100

    Update Exercise5.java
    Exercise Completed

--- src/arrays/exercises/Exercise5.java
+++ src/arrays/exercises/Exercise5.java
@@ -1,5 +1,7 @@
 package arrays.exercises;
 
+import java.util.Scanner;
+
 /**
  * Exercise 5: Linear Search
  * TODO:
@@ -12,6 +14,28 @@ public class Exercise5
 {
     public static void main(String[] args)
     {
-        // TODO: Implement exercise
+        int[] arr = { 10, 20, 13, 40, 35, 29 }; //Created an array of integers
+
+        Scanner scanner = new Scanner(System.in); // Value asked from the user
+
+        System.out.print("Enter a number: ");
+        int key = scanner.nextInt();
+
+        System.out.println("You entered: " + key);
+
+        scanner.close();
+         
+        int index = -1;                         // Implemented linear search.
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] == key) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1)                                          // Printed index if found or "Not found".
+            System.out.println("Element found at index " + index);
+        else
+            System.out.println("Element not found");
     }
 }
